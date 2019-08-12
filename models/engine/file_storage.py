3ba5f32d121d79a0a1ee0586bd5e7ececce47e22
yawzyag <yesid.yag@gmail.com>
2019-08-12 22:23:27 +0000

    [Feature] Update FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,11 +20,18 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
+        if cls is not None:
+            dict = {}
+            for key, val in self.__objects.items():
+                my_key = key.split(".")
+                if cls.__name__ == my_key[0]:
+                    dict.update({key: val})
+            return dict
         return self.__objects
 
     def new(self, obj):
@@ -55,3 +62,9 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        for key, val in self.__objects.items():
+            if obj == val:
+                del self.__objects[key]
+                break
