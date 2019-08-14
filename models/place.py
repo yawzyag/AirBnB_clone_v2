d33587baf3237b466778a4a3b134d2a7716116c5
yawzyag <yesid.yag@gmail.com>
2019-08-14 17:17:00 +0000

    [feature] adding files for 8

--- models/place.py
+++ models/place.py
@@ -5,7 +5,7 @@ from sqlalchemy import Table, Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
         city_id: city id
@@ -24,11 +24,11 @@ class Place(BaseModel):
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
--- models/user.py
+++ models/user.py
@@ -18,5 +18,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-#        places = relationship("Place", backref="user")
+    places = relationship("Place", backref="user")
 #        reviews = relationship("Review", backref="user")
