29478f953b2193b70a18297cb98dc38f8de8d838
yawzyag <yesid.yag@gmail.com>
2019-08-19 21:17:03 +0000

    [Fix] adding sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,2 +1,12 @@
-#!/usr/bin/python3
-"""This is the console for AirBnB"""
+#!/usr/bin/env bash
+"""This is the script for AirBnB"""
+
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
+echo "<h1>Holberton School</h1>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "41i " /etc/nginx/sites-available/default 
+sudo service nginx restart
