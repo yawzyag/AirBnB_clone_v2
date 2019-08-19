5ac4b1352265e844799dd6b09386773cd8f82ba6
yawzyag <yesid.yag@gmail.com>
2019-08-19 21:38:40 +0000

    [Fix] coment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ sudo mkdir -p /data/web_static/shared
 echo "<h1>Holberton School</h1>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "42i location /hbnb_static/ {\n\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enable/default 
+sudo sed -i "40i location /hbnb_static/ {\n\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enable/default 
 sudo service nginx restart
