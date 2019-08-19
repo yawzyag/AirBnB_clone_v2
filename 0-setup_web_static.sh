5e2a659f440e2008891312323394b808f2169f63
yawzyag <yesid.yag@gmail.com>
2019-08-19 21:36:17 +0000

    [Fix] coment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-"""This is the script for AirBnB"""
-
+#This is the script for AirBnB
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
@@ -8,5 +7,5 @@ sudo mkdir -p /data/web_static/shared
 echo "<h1>Holberton School</h1>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "41i " /etc/nginx/sites-available/default 
+sudo sed -i "42i location /hbnb_static/ {\n\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enable/default 
 sudo service nginx restart
