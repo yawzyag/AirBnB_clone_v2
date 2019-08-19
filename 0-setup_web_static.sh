a00146b7ccade9ec5a92d64abd62a640d87730b1
yawzyag <yesid.yag@gmail.com>
2019-08-19 22:31:58 +0000

    [Fix] permisions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-#This is the script for AirBnB
-sudo apt-get update -y
+# testing my script
+sudo apt-get update
 sudo apt-get install nginx -y
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared
-echo "<h1>Holberton School</h1>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "40i location /hbnb_static/ {\n\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enable/default 
-sudo service nginx restart
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "test" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown ubuntu:ubuntu -R /data/
+sed -i '52i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+service nginx restart
