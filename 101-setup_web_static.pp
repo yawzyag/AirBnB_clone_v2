38234b9ba23f6ebcaeefc0f93ffd7e984f536847
yawzyag <yesid.yag@gmail.com>
2019-08-20 01:26:23 +0000

    [Puppet] adding file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -4,30 +4,28 @@ command  => 'sudo apt update',
 provider => shell,
 } -> package {'nginx':
 ensure => 'installed',
-} -> file { '/data/web_static/shared/':
-    ensure => 'directory',
-} -> file { '/data/web_static/releases/test/':
-    ensure => 'directory',
+} ->file { [ '/data/',
+  '/data/web_static/',
+  '/data/web_static/releases/',
+  '/data/web_static/shared/',
+  '/data/web_static/releases/test/', ]:
+  ensure => directory,
 } -> file { '/data/web_static/releases/test/index.html':
-      content => "test",
-} ->   file { '/data/web_static/current':
-    ensure => 'link',
-    target => '/data/web_static/releases/test/',
-  } -> file { '/data/':
-    ensure    => directory,
-    owner     => 'ubuntu',
-    group      => 'ubuntu',
-    require     => [ User['ubuntu'], Group['ubuntu'], ],
-    recurse    => true,
-} -> -> file_line { 'Edit redirect':
-ensure => present,
-path   => '/etc/nginx/sites-enable/default',
-line   => "	#error_page 404 /404.html;
-       location /hbnb_static/ {
-       		alias /data/web_static/current/;
-	}",
-match  => '^\t#error_page 404 /404.html;',
-} ->  exec { 'restart':
+      ensure  => 'present',
+      content => 'testing',
+}
+-> exec {'link':
+command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
+provider => shell,
+}
+-> exec {'chown':
+command  => 'chown ubuntu:ubuntu -R /data/',
+provider => shell,
+}
+-> exec {'sed':
+command  => "sed -i '52i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/defaul",
+provider => shell,
+}
+-> exec { 'restart':
 command => '/usr/sbin/service nginx restart',
 }
-
