f022ec4e885ff0a0763b2e1ca27ddea6b283132e
yawzyag <yesid.yag@gmail.com>
2019-09-03 17:29:43 +0000

    [Feature] adding 1 file

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def route_HBNB():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
