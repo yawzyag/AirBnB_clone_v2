b206f89782acfae8b8786908278ee0d74e298071
yawzyag <yesid.yag@gmail.com>
2019-09-03 17:26:20 +0000

    [Feature] adding the 0 file

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
