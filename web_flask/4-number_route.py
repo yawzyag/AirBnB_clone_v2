11a7936cc9637d4df42ab8eb902a4cb92e50f258
yawzyag <yesid.yag@gmail.com>
2019-09-03 17:56:26 +0000

    [Feature] adding 4 file

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def route_HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def text_HBNB(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def pyt_HBNB(text="is cool"):
+    s = """Python {zzz}"""
+    return s.format(zzz=text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def int_HBNB(n):
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
