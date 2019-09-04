3f1ae2378ef8ad926b0c484b6e2fa107d2f296e0
yawzyag <yesid.yag@gmail.com>
2019-09-04 00:10:29 +0000

    [Fix] adding file html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,15 +9,25 @@ app.url_map.strict_slashes = False
 
 @app.route('/states_list')
 def hello_HBNB():
+    dicty = {}
     all_states = storage.all(State)
     for state_id, state in all_states.items():
-        print(state_id)
-        print(state)
-    return 'Hello HBNB!'
+        list2 = state_id.split(".")[1]
+        list1 = state.name
+        dicty.update({list1: list2})
+
+    list1 = []
+    list2 = []
+    for key in sorted(dicty):
+        list1.append(key)
+        list2.append(dicty[key])
+    return render_template('7-states_list.html', dicty=zip(list1, list2))
+
 
 @app.teardown_appcontext
-def teardown_db():
+def home_teradown(exit):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
