0aad3d967f126a701214af7ed35a1761905d4ad6
yawzyag <yesid.yag@gmail.com>
2019-09-05 00:33:13 +0000

    [Fix] advance task

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -3,12 +3,14 @@
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
+from models.user import User
 from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/states')
+@app.route('/hbnb')
 def hello_HBNB():
     all_states = storage.all(State)
     states = list(all_states.values())
@@ -16,8 +18,16 @@ def hello_HBNB():
     all_amenities = storage.all(Amenity)
     amenities = list(all_amenities.values())
 
-    return render_template('10-hbnb_filters.html',
-                           states=states, amenities=amenities)
+    all_places = storage.all(Place)
+    places = list(all_places.values())
+
+    all_users = storage.all(User)
+    users = list(all_users.values())
+   
+
+    return render_template('100-hbnb.html',
+                           states=states, amenities=amenities,
+                           places=places, users=users)
 
 
 @app.teardown_appcontext
